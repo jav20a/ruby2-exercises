4c075c5b39c14161629b1ae0456ad0949a588e64
Jesus Abarca <jesusa@172-0-7-176.lightspeed.brhmal.sbcglobal.net>
2014-12-15 17:32:11 -0600

    adding old_roman_numerals.rb

--- old_roman_numerals.rb
+++ old_roman_numerals.rb
@@ -1,2 +1,34 @@
-class
+def find_divisor (dividend, divisor)
+  quotient = dividend / divisor
+  return quotient
+end
+
+def find_remainder (dividend, quotient, divisor)
+  remainder = dividend % (quotient * divisor)
+  return remainder
+end
+
+# roman_symbols = ['I', ]
+
+# ask for the number
+while true
+  puts 'Please type a whole number between 1 and 3000.'
+  typed_number = gets.chomp
+
+  # check length
+
+  decimal_places = typed_number.length
+  dividend = typed_number.to_i
+  if dividend.between?(1, 300)
+
+    decimal_places.times do
+      decimal_places = decimal_places - 1
+      divisor = 10 ** decimal_places
+      quotient = find_divisor dividend, divisor
+      puts quotient
+      dividend = find_remainder dividend, quotient, divisor
+    end
+    return 'finished'
+    # construct and return the roman numeral
+  end
 end
