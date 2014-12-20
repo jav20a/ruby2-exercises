d9f91f45d8b9ce3b83e4026cd0ad398ddba3a73b
J Jesús Abarca <jav2.0a@gmail.com>
2014-12-19 18:55:22 -0600

    using enumerable method as per Sean suggestion

--- 99_bottles_of_beer.rb
+++ 99_bottles_of_beer.rb
@@ -1,15 +1,7 @@
-number_of_bottles = 99
-while number_of_bottles != 'no more'
-    puts number_of_bottles.to_s + ' bottles of beer on the wall, ' + number_of_bottles.to_s + ' bottles of beer.'
-    
-    if number_of_bottles == 1
-        number_of_bottles = 'no more'
-    else
-        number_of_bottles = number_of_bottles - 1
-    end
-    puts 'Take one down and pass it around, ' + number_of_bottles.to_s + ' bottles of beer on the wall.'
-    puts
+(1..99).reverse_each do |number|
+    puts "#{number} bottles of beer on the wall, #{number} bottles of beer."
+    puts "Take one down and pass it around, #{number == 1 ? 'No more' : (number-1) } bottles of beer on the wall."
+    puts ''
 end
-
 puts 'No more bottles of beer on the wall, no more bottles of beer.'
 puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
